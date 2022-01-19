869c0819a7f80631e728dc381c72be861198b194
Cico96 <40207930+Cico96@users.noreply.github.com>
2022-01-19 18:31:18 +0100

    add user attruibute

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -5,8 +5,70 @@ import { Attachment } from "./attachment";
 @Entity()
 export class User extends BaseEntityClass {
 
-    @Column()
+    @Column({
+        type: "varchar"
+    })
     name: string;
+
+    @Column({
+        type: "varchar"
+    })
+    surname: string;
+
+    @Column({
+        type: "varchar"
+    })
+    username: string;
+
+    @Column({
+        type: "varchar"
+    })
+    password: string;
+
+    @Column({
+        type: "varchar"
+    })
+    city: string;
+
+    @Column({
+        type: 'timestamp'
+    })
+    date: string;
+
+    @Column({
+        type: "varchar"
+    })
+    birthplace: string;
+
+    @Column({
+        type: "varchar"
+    })
+    gender: string;
+
+    @Column({
+        type: "varchar"
+    })
+    contractType: string;
+
+    @Column({
+        type: "float"
+    })
+    grossAnnualSalary: number;
+
+    @Column({
+        type: "varchar"
+    })
+    businessName: string;
+
+    @Column({
+        type: "varchar"
+    })
+    vatNumber: string;
+
+    @Column({
+        type: "varchar"
+    })
+    attorny: string;
 
 
     @OneToMany(() => Attachment, f => f.user, {nullable: true})
